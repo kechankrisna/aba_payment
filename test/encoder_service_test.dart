7b24778bcb16e235cab8f30fb1e92ed2eac96e74
kechankrisna <ke.chankrisna168@gmail.com>
2023-02-13 14:47:43 +0700

    add EncoderService and test

---
+++ test/encoder_service_test.dart
@@ -0,0 +1,44 @@
+// import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:aba_payment/service/encoder_service.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // const MethodChannel channel = MethodChannel('aba_payment');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  test("EncoderService.base46_encode and base46_decode map", () {
+    List<Map<String, dynamic>> input = [
+      {"name": "ទំនិញសាកល្បង", "quantity": 1, "price": 6}
+    ];
+    var encoded = EncoderService.base46_encode(input);
+    print("encoded $encoded");
+    var decoded = EncoderService.base46_decode(encoded);
+    print("decoded $decoded");
+
+    expect(input, decoded);
+  });
+
+  test("EncoderService.base46_encode and base46_decode String", () {
+    final input = "ទំនិញសាកល្បង";
+    var encoded = EncoderService.base46_encode(input);
+    print("encoded $encoded");
+    var decoded = EncoderService.base46_decode(encoded);
+    print("decoded $decoded");
+
+    expect(input, decoded);
+  });
+
+  test("EncoderService.base46_encode and base46_decode null", () {
+    final input = null;
+    var encoded = EncoderService.base46_encode(input);
+    print("encoded $encoded");
+    var decoded = EncoderService.base46_decode(encoded);
+    print("decoded $decoded");
+
+    expect(input, decoded);
+  });
+}
