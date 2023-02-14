1f785efd457fa69ddea35a0342d0eef1b283598c
kechankrisna <ke.chankrisna168@gmail.com>
2023-02-14 15:08:44 +0700

    name

--- lib/services/payway_client_response_service.dart
+++ lib/services/payway_client_response_service.dart
@@ -1,10 +1,65 @@
-class CreateTransactionResponseMessage {
+class PaywayCreateTransactionResponse {
+  int? status;
+  String? description;
+  String? qrString;
+  String? qrImage;
+  String? abapayDeeplink;
+  String? appStore;
+  String? playStore;
+  String? rawcontent;
+
+  PaywayCreateTransactionResponse({
+    this.status,
+    this.description,
+    this.qrString,
+    this.qrImage,
+    this.abapayDeeplink,
+    this.appStore,
+    this.playStore,
+    this.rawcontent,
+  });
+
+  factory PaywayCreateTransactionResponse.fromMap(Map<String, dynamic> map) {
+    int _statusCode = -1;
+    if (map["status"] is int) {
+      _statusCode = map["status"];
+    }
+    if (map["status"] is Map) {
+      _statusCode = int.tryParse("${map["status"]["code"]}")!;
+    }
+    return PaywayCreateTransactionResponse(
+      status: _statusCode,
+      description: map["description"],
+      qrString: map["qrString"],
+      qrImage: map["qrImage"],
+      abapayDeeplink: map["abapay_deeplink"],
+      appStore: map["app_store"],
+      playStore: map["play_store"],
+      rawcontent: null,
+    );
+  }
+  Map<String, dynamic> toMap() => {
+        "status": status,
+        "description": description,
+        "qrString": qrString,
+        "qrImage": qrImage,
+        "abapay_deeplink": abapayDeeplink,
+        "app_store": appStore,
+        "play_store": playStore,
+        "rawcontent": rawcontent,
+      };
+
+  String get message =>
+      PaywayCreateTransactionResponseMessage.of(status!).message;
+}
+
+class PaywayCreateTransactionResponseMessage {
   int value;
 
-  CreateTransactionResponseMessage._(this.value);
+  PaywayCreateTransactionResponseMessage._(this.value);
 
-  factory CreateTransactionResponseMessage.of(int value) =>
-      CreateTransactionResponseMessage._(value);
+  factory PaywayCreateTransactionResponseMessage.of(int value) =>
+      PaywayCreateTransactionResponseMessage._(value);
 
   String get message {
     switch (value) {
