d0555da99d3b78bae92b5ce5847eb32fc49ca919
kechankrisna <ke.chankrisna168@gmail.com>
2023-02-14 18:29:24 +0700

    generate test

--- test/payway_transaction_service_test.dart
+++ test/payway_transaction_service_test.dart
@@ -47,7 +47,8 @@ void main() {
       var createResponse =
           await service.createTransaction(transaction: _transaction);
 
-      expect(createResponse.abapayDeeplink != null, true);
+      expect(createResponse.abapayDeeplink != null, true,
+          reason: "the deeplink should be a string according to docs");
 
       var checkResponse = await service.checkTransaction(
           transaction: PaywayCheckTransaction(
@@ -55,7 +56,44 @@ void main() {
         reqTime: service.uniqueReqTime(),
       ));
 
-      expect(checkResponse.status  == 2, true);
+      expect(checkResponse.status == 2, true,
+          reason: "the new transaction created should be pending or status 2");
+    });
+
+    test("generate checkout uri for a transaction then check status pending",
+        () async {
+      final service = PaywayTransactionService.instance!;
+      final tranID = service.uniqueTranID();
+
+      var _transaction = PaywayCreateTransaction(
+          amount: 6.00,
+          items: [
+            PaywayTransactionItem(name: "ទំនិញ 1", price: 1, quantity: 1),
+            PaywayTransactionItem(name: "ទំនិញ 2", price: 2, quantity: 1),
+            PaywayTransactionItem(name: "ទំនិញ 3", price: 3, quantity: 1),
+          ],
+          reqTime: service.uniqueReqTime(),
+          tranId: tranID,
+          email: 'support@mylekha.app',
+          firstname: 'Miss',
+          lastname: 'My Lekha',
+          phone: '010464144',
+          option: ABAPaymentOption.abapay_deeplink,
+          shipping: 0.0,
+          returnUrl: "https://stage.mylekha.app");
+      String checkoutApiUrl =
+          "http://localhost/api/v1/integrate/payway/checkout_page";
+      var webURI = await service.generateTransactionCheckoutURI(
+          transaction: _transaction, checkoutApiUrl: checkoutApiUrl);
+
+      expect(
+          webURI.queryParameters['items'],
+          EncoderService.base46_encode(
+              _transaction.items.map((e) => e.toMap()).toList()),
+          reason: "encoded items should be equal");
+
+      expect(webURI.authority.isNotEmpty, true,
+          reason: "the uri should be generated");
     });
   });
 }
